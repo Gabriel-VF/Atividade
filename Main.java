d7144628ba941de92ed99870161287a75d3c26e5
Gabriel Veríssimo <vergabriel07@gmail.com>
2026-02-15 01:35:16 +0000

    Update and rename Main to Main.java
    Loop infinito adicionado. Nome consertado

--- Main.java
+++ Main.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		char op;
+		char op = ' ';
 		double resultado = 0;
 		double a, b;
 		Scanner scanner = new Scanner(System.in);
 
+		while (op != 'c') {
 		System.out.print("Insira operação a ser feita [+-/*]: ");
 		op = scanner.next().charAt(0);
 		scanner.nextLine();
@@ -14,25 +15,25 @@ public class Main {
 		a = scanner.nextDouble();
 		System.out.print("Insira número 2: ");
 		b = scanner.nextDouble();
-
-		switch(op){
-			case '+':
-			resultado = a + b;
-			break;
-			case '-':
-			resultado = a - b;
-			break;
-			case '*':
-			resultado = a * b;
-			break;
-			case '/':
-			resultado = a / b;
-			break;
-			default:
-			resultado = 0;
-			System.out.println("Operação inválida");
+			switch (op) {
+				case '+':
+					resultado = a + b;
+					break;
+				case '-':
+					resultado = a - b;		
+					break;
+				case '*':
+					resultado = a * b;
+					break;
+				case '/':
+					resultado = a / b;
+					break;
+				default:
+					resultado = 0;
+					System.out.println("Operação inválida");
+			}
+			System.out.println("Resultado: " + resultado);
 		}
-		System.out.println("Resultado: " + resultado);
 		scanner.close();
 	}
 }
